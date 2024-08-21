fa3f696fe9ec1b29dff9a55d488c531aac629026
Ori Pomerantz <qbzzt1@gmail.com>
2024-08-21 08:10:14 -0500

    Add garnet p2p

--- docker-compose/config/garnet/run-consensus-layer.sh
+++ docker-compose/config/garnet/run-consensus-layer.sh
@@ -12,4 +12,5 @@ cd /data
   --rpc.addr=0.0.0.0 \
   --rpc.port=8547 \
   --rpc.enable-admin \
+  --p2p.bootnodes=enr:-J64QM3nCEvp4MQhkZrjnst7X9pzUL7ikW3jL5VXsHMbqw0PCDxEctBPAOGluJgg4-2xk4WeDRPV60_zl52_9cHSceiGAZFS5fg8gmlkgnY0gmlwhCLxekOHb3BzdGFja4SthQEAiXNlY3AyNTZrMaEDb4HiEkpCzMLwzTAGG6pqwxX-7cAKLI_FzZz8EuycauODdGNwgiQGg3VkcIIkBg,enr:-J64QJZJo5gi8EaDBk_qWvhTe1ioAFiSEriXN0XlCEKcpiSSASBBITr1v8vEMBZA4PdHjdwFYlnclDgk4YDLBuYV-oKGAY6lSMgZgmlkgnY0gmlwhAP7R1eHb3BzdGFja4SthQEAiXNlY3AyNTZrMaECvgC27BLpm7ZNcclZOYDJhcTFuuTsJzfvafzbHPlllwaDdGNwgiQGg3VkcIIkBg \
   --rollup.config=/config/garnet/rollup.json
